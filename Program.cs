66fd3d6339e34aa06431339e796c5c90d7ffbffc
haribo009 <karinazhdanovich95@gmail.com>
2024-06-27 09:13:37 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using GuitarExtasy.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<GuitarExtasyContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GuitarExtasyContext") ?? throw new InvalidOperationException("Connection string 'GuitarExtasyContext' not found.")));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Login";
+        options.AccessDeniedPath = "/AccessDenied";
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
